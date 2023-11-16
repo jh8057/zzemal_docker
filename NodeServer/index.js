ce85241edb8ac5a83fd424ef4869c478789193c9
jh8057 <luckysymbol13@gmail.com>
2023-11-17 01:43:02 +0900

    [node] 이미지 저장
    - static 접근 설정
    - upload 설정 ( multer )

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = 3001;
+
+//body-parser 설정
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //cors 설정
 const cors = require("cors");
@@ -8,7 +14,6 @@ const cors = require("cors");
 const whitelist = [
   "http://localhost:3000",
   "http://localhost:3001, http://localhost:3002",
-  ,
   "http://localhost:5173",
 ];
 
@@ -18,6 +23,9 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+
+//static 설정
+app.use("/images", express.static("images"));
 
 const router = require("./router");
 app.use("/api", router);
--- NodeServer/router/menu/index.js
+++ NodeServer/router/menu/index.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const router = express.Router();
-
+const multer = require("multer");
 const menuList = require("./menuList.json");
+//multer 설정
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+      console.log("file", file, req);
+      cb(null, file.originalname);
+    },
+  }),
+});
 
 router.get("/", (req, res) => {
   console.log("menuList", menuList);
   res.json(menuList);
+});
+
+router.post("/upload", upload.single("image"), (req, res) => {
+  res.json(true);
 });
 
 module.exports = router;
