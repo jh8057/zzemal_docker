5d9cab9f6d71ce29c21174aabb61b06e5c77e51a
jh8057 <luckysymbol13@gmail.com>
2023-12-14 23:28:42 +0900

    [vue] rive

--- VueServer/src/views/EnjoyView.vue
+++ VueServer/src/views/EnjoyView.vue
@@ -1,9 +1,42 @@
 <template>
-  <h1>준비중..</h1>
+  <div>
+    <h1>놀거리</h1>
+    <canvas id="riveC" width="400" height="400"></canvas>
+  </div>
 </template>
 
-<script>
-export default {}
-</script>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { Rive } from '@rive-app/canvas'
 
-<style></style>
+onMounted(() => {
+  console.log('mounted')
+
+  const reRender = () => {
+    new Rive({
+      canvas: document.getElementById('riveC') as HTMLCanvasElement,
+      src: '/src/assets/riveIcon.riv',
+      autoplay: true
+    })
+  }
+  // loop reRender
+  setInterval(() => {
+    reRender()
+  }, 2000)
+
+  // let renderer: any = null
+  // const reRender = () => {
+  //   if (false) {
+  //     cancelAnimationFrame(renderer)
+  //   }
+
+  //   new Rive({
+  //     canvas: document.getElementById('riveC') as HTMLCanvasElement,
+  //     src: '/src/assets/riveIcon.riv',
+  //     autoplay: true
+  //   })
+  //   renderer = requestAnimationFrame(reRender)
+  // }
+  // requestAnimationFrame(reRender)
+})
+</script>
